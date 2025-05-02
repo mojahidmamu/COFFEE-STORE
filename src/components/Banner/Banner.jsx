6ad01efd12373f01617d177a18faa49393bc1116
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-02 08:45:48 +0600

    added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            this is banner... 
-            <h2>mojahid. bin yousof...</h2>
+            
         </div>
     );
 };

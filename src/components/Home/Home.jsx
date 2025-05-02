f29568615be38ff6d0dad2ddaddd268828bbda92
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-02 08:55:11 +0600

    added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Navbar from '../Navbar/Navbar';
 
 const Home = () => {
     return (
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="flex flex-row justify-center items-center gap-3 bg-red-950">
+      <img
+        className="w-10 h-10 my-3"
+        src="../../assets/images/more/logo1.png"
+        alt=""
+      />
+      <h2 className="font-bold text-xl text-[#FFFFFF]">Espresso Emporium</h2>
+    </div>
+  );
 };
 
 export default Navbar;

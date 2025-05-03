511e9d49e6dbf3e39faaff96b3f345370b19fec4
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-03 09:56:25 +0600

    added

--- src/components/PopularProductSection/PopularProductSection.jsx
+++ src/components/PopularProductSection/PopularProductSection.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import Prodcut1 from "../../assets/all_image/images"
+import Prodcut1 from "../../assets/all_image/images/Products/1.png"
+import Prodcut2 from "../../assets/all_image/images/Products/2.png"
+import Prodcut3 from "../../assets/all_image/images/Products/3.png"
+import Prodcut4 from "../../assets/all_image/images/Products/4.png"
+import Prodcut5 from "../../assets/all_image/images/Products/5.png"
+import Prodcut6 from "../../assets/all_image/images/Products/6.png"
 
 const PopularProductSection = () => {
     return (

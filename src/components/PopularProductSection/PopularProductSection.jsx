51f135bccf0b535e1803c5a11c76d79f365790be
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-03 09:54:35 +0600

    added

--- src/components/PopularProductSection/PopularProductSection.jsx
+++ src/components/PopularProductSection/PopularProductSection.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Prodcut1 from "../../assets/all_image/images"
 
 const PopularProductSection = () => {
     return (
         <div>
-            PopularProductSection....
+            {/* Product 01 */}
+            <div className='bg-[#F5F4F1]'>
+                {/* image div */}
+                <div></div>
+                {/* address div */}
+                <div></div>
+                {/* button div */}
+                <div></div>
+            </div>
         </div>
     );
 };

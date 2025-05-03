3f1735c7c258543945ae295d6cac04342cb01329
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-03 08:48:54 +0600

    added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,24 +1,26 @@
 import React from "react";
+import BannerImage from "../../assets/all_image/images/more/6.jpeg";
 
 const Banner = () => {
   return (
     <div
       className="hero min-h-screen"
       style={{
-        backgroundImage:
-          "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+        backgroundImage: `url(${BannerImage})`,
       }}
     >
       <div className="hero-overlay"></div>
       <div className="hero-content text-neutral-content text-center">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h2 className="mb-3 text-xl font-bold">
+            Would you like a Cup of Delicious Coffee?
+          </h2>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            It's coffee time - Sip & Savor - Relaxation in every sip! Get the
+            nostalgia back!! Your companion of <br />
+            every moment!!! Enjoy the beautiful moments and make them memorable.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-warning">Learn More</button>
         </div>
       </div>
     </div>

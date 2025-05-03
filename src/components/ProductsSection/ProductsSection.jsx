48053d09422f8db36783a0e8172774a8e100ab05
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-03 09:36:32 +0600

    added

--- src/components/ProductsSection/ProductsSection.jsx
+++ src/components/ProductsSection/ProductsSection.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import ProductBgImage from "../../assets/all_image/images/more/1.png"
 
 const ProductsSection = () => {
-    return (
-        <div>
-            i am in ProductsSection...             
-        </div>
-    );
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `url(${ProductBgImage})`,
+      }}
+    >
+      i am in ProductsSection...
+    </div>
+  );
 };
 
 export default ProductsSection;

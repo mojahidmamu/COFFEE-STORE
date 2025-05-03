2846b37e8e34cff89b9d4470fc8bc8b804b34ae8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-03 08:40:42 +0600

    added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import NabLogo from "../../assets/all_image/images/more/logo1.png"
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row justify-center items-center gap-3 bg-red-950">
+    <div className="flex flex-row justify-center items-center gap-3 bg-red-900">
       <img
         className="w-10 h-10 my-3"
-        src="../../assets/images/more/logo1.png"
+        src={NabLogo}
         alt=""
       />
       <h2 className="font-bold text-xl text-[#FFFFFF]">Espresso Emporium</h2>

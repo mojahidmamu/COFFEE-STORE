61a9455fcd0c009aea4c26a1974b2974e4faa7e5
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-05-03 09:11:05 +0600

    added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../Banner/Banner"; 
 import Card from "../Card/Card";
+import ProductsSection from "../ProductsSection/ProductsSection";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Card></Card>
+      <ProductsSection></ProductsSection>
     </div>
   );
 };
---
+++ src/components/ProductsSection/ProductsSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductsSection = () => {
+    return (
+        <div>
+            i am in ProductsSection...             
+        </div>
+    );
+};
+
+export default ProductsSection;
